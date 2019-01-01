0abbefc4f617fe1dacd2c799b11a6abc0382cd7b
ale <alejandro.borrego.delgado@gmail.com>
2019-01-01 23:09:08 +0100

    create-react-app

--- src/index.js
+++ src/index.js
@@ -1,70 +1,12 @@
-const SerialPort = require("serialport");
-const Readline = require("@serialport/parser-readline");
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-let parameterRead;
-let next = 0;
-const commands = ["preassure", "humidity", "temperature"];
+ReactDOM.render(<App />, document.getElementById('root'));
 
-const readOne = parameter => {
-  parameterRead = parameter;
-  let command;
-  switch (parameter) {
-    case "preassure":
-      command = "GP";
-      break;
-    case "temperature":
-      command = "GT";
-      break;
-    case "humidity":
-      command = "GH";
-      break;
-    case "name":
-      command = "GN";
-      break;
-    default:
-      console.log("Unknown command");
-  }
-
-  console.log("command", command);
-
-  port.write(`${command}\n\n`, function(err) {
-    console.log("written");
-    if (err) {
-      console.log("Error on write: ", err.message);
-    }
-  });
-};
-
-// const readAll = () => {
-//   setTimeout(() => readOne("preassure"), 1000);
-//   // setTimeout(() => readOne("humidity"), 2000);
-//   // setTimeout(() => readOne("temperature"), 3000);
-//   // setTimeout(() => readOne("name"), 4000);
-//   // setTimeout(() => readAll(), 5000);
-// };
-
-const port = new SerialPort("/dev/ttyACM0", {
-  baudRate: 115200
-});
-
-port.on("error", function(err) {
-  console.log("Error: ", err.message);
-  readOne(commands[next]);
-});
-
-const parser = new Readline();
-port.pipe(parser);
-parser.on("data", data => {
-  // console.log("read");
-  console.log(`${parameterRead}: ${data}`);
-  next = next < 2 ? next + 1 : 0;
-  setTimeout(() => readOne(commands[next]), 1000);
-});
-
-// port.on("readable", function() {
-//   console.log("Data:", port.read());
-// });
-//
-// readOne("preassure")
-
-readOne(commands[next]);
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
